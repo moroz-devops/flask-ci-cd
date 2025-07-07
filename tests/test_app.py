c2818a53dc596c6e11f18d7a3c99e712162b0f95
moroz-devops <p.moroz.devops@gmail.com>
2025-07-07 13:01:37 +0300

    Fixed test_app, __init__.py added

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,4 +7,4 @@ from app import app
 def test_status():
     response = app.test_client().get('/status')
     assert response.status_code == 200
-    assert response.get_json == {'status':'ok'}
+    assert response.get_json() == {'status':'ok'}
