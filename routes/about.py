774f4865ec3c5641ac26e8532fcbaef189eac19c
moroz-devops <p.moroz.devops@gmail.com>
2025-07-10 12:31:09 +0300

    test

--- routes/about.py
+++ routes/about.py
@@ -1,6 +1,6 @@
 def about_route():
     return '''
-    <h1></h1>
+    <h1>About this site</h1>
     <p>This is my first Flask project with continious integration and deployment.<p>
-    <a href="/"></a>
+    <a href="/">Back to main page</a>
     '''
