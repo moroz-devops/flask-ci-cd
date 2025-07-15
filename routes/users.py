d91db3ed33aa65fa09f0a07025b5324bd53ecb48
moroz-devops <p.moroz.devops@gmail.com>
2025-07-16 02:35:43 +0300

    Added postgre database

--- routes/users.py
+++ routes/users.py
@@ -1,16 +1,10 @@
 from flask import Blueprint, render_template
-import sqlite3
+from models import User
 
 users_bp = Blueprint('users', __name__)
 
 @users_bp.route('/users')
-
 def users_route():
-    conn = sqlite3.connect('site.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT name FROM users')
-    users_list = cursor.fetchall()
-    conn.close()
-    
-    return render_template('users.html', users=users_list)
+    users = User.query.all()
+    return render_template('users.html', users=users)
 
