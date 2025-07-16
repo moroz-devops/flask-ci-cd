3d0e9c2dff5acc0e5969a523be7dd85a7a4c98d0
moroz-devops <p.moroz.devops@gmail.com>
2025-07-16 08:03:54 +0300

    Fix (/users): replace raw SQL with ORM query

--- models.py
+++ models.py
@@ -4,4 +4,7 @@ from database import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
+    
+    def __repr__(self):
+        return f'<User {self.name}>'
 
