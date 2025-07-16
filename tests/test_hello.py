edb5667d9be1b068fe310cc54e9dcd415e4e8b53
moroz-devops <p.moroz.devops@gmail.com>
2025-07-16 09:49:49 +0300

    Added config file for app

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -2,7 +2,9 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from app import app
+from app import create_app
+
+app = create_app()
 
 def test_hello_get():
     client = app.test_client()
--- tests/test_status.py
+++ tests/test_status.py
@@ -2,7 +2,9 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from app import app
+from app import create_app
+
+app = create_app()
 
 def test_status_page():
     client = app.test_client()
